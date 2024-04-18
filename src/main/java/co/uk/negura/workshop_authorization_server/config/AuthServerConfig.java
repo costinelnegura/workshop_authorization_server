7fcd8974c5a5910cc3e8e75b58ed766439889da4
costinelnegura <pigeons-conduit0n@icloud.com>
2024-04-18 10:26:36 +0100

    initial commit

--- src/main/java/co/uk/negura/workshop_authorization_server/config/AuthServerConfig.java
+++
@@ -1,2 +0,0 @@
-package co.uk.negura.workshop_authorization_server.config;public class AuthServerConfig {
-}
--- src/test/java/co/uk/negura/workshop_authorization_server/WorkshopAuthorizationServerApplicationTests.java
+++ src/test/java/co/uk/negura/workshop_authorization_server/WorkshopAuthorizationServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WorkshopAuthorizationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
